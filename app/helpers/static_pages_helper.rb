2c093b4bfed1bfa5199be87d1375665238339a96
Andrew Wu <wujuichang@gmail.com>
2014-08-10 08:58:29 -0700

    Move select options into a helper

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -8,4 +8,37 @@ module StaticPagesHelper
       raw(format)
     end
   end
+
+  def options_for_year
+    options_for_select(Model.pluck(:year).uniq.sort)
+  end
+
+  def options_for_manufacturer
+    options_for_select(
+      Manufacturer.order(:name).map { |manufacturer|
+        years = "#{manufacturer.models.pluck(:year).uniq}"
+        [manufacturer.name, { 'data-years' => years }]
+      }
+    )
+  end
+
+  def grouped_options_for_model
+    grouped_options_for_select(
+      Manufacturer.order(:name).map { |manufacturer|
+        [manufacturer.name, manufacturer.models.map { |model|
+          [model.name, model.id, { 'data-year' => model.year }]
+        }]
+      }
+    )
+  end
+
+  def grouped_options_for_size
+    grouped_options_for_select(
+      Model.order(:name).map { |model|
+        [model.full_name, model.sizes.map { |size|
+          ["%g" % size.frame_size, size.id]
+        }]
+      }
+    )
+  end
 end
